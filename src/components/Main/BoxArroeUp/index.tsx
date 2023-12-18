880587c036f59089d333f1b2049679eeb9215acc
Joabe Santos <joabesilva1233@gmail.com>
2023-12-18 15:47:53 -0300

    fix: arrow up

--- src/components/Main/BoxArroeUp/index.tsx
+++ src/components/Main/BoxArroeUp/index.tsx
@@ -6,7 +6,7 @@ import upArrow from '../../../../public/assets/up-arrow.svg'
 export const BoxArrowUp = () => {
     return (
         <div id="arrouUp" className={styles.box_arrouUp}>
-            <a href="#top"><Image className={styles.arrowUp} src={upArrow} alt="Arrow Up" /></a>
+            <a href="#top" className={styles.linkArrow}><Image className={styles.arrowUp} src={upArrow} alt="Arrow Up" /></a>
         </div>
     )
 }
