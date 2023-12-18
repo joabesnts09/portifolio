4164b22875d40d40f9c445571a53d389eb1096de
Joabe Santos <joabesilva1233@gmail.com>
2023-12-18 12:26:34 -0300

    fix: window error

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -1,28 +1,30 @@
-
 import styles from '../components/Header/style.module.scss'
 import style from '../components/Main//BoxArroeUp/style.module.scss'
 
 export const useScroll = () => {
-    window.addEventListener('scroll', () => {
-        const header = document.querySelector('#header') as HTMLAnchorElement
-        const arrouUp = document.querySelector('#arrouUp') as HTMLAnchorElement
-
-        const sectionAbout = document.querySelector('#about') as HTMLAnchorElement
-        const sectionSkills = document.querySelector('#skill') as HTMLAnchorElement
-
-        header.classList.toggle(styles.scroll, window.scrollY > 0)
-        arrouUp.classList.toggle(style.scrollArrow, window.scrollY > 600)
-        
-        if(window.scrollY > 100 && window.scrollY < 500) {
-            sectionAbout.classList.add(styles.actived)
-            sectionSkills.classList.remove(styles.actived)
-        } else if (window.scrollY >= 500) {
-            sectionAbout.classList.remove(styles.actived)
-            sectionSkills.classList.add(styles.actived)
-        } else {
-            sectionAbout.classList.remove(styles.actived)
-            sectionSkills.classList.remove(styles.actived) 
-        }
-        
-    })
+    if (typeof window !== "undefined") {
+        window.addEventListener('scroll', () => {
+            const header = document.querySelector('#header') as HTMLAnchorElement
+            const arrouUp = document.querySelector('#arrouUp') as HTMLAnchorElement
+    
+            const sectionAbout = document.querySelector('#about') as HTMLAnchorElement
+            const sectionSkills = document.querySelector('#skill') as HTMLAnchorElement
+    
+            header.classList.toggle(styles.scroll, window.scrollY > 0)
+            arrouUp.classList.toggle(style.scrollArrow, window.scrollY > 600)
+            
+            if(window.scrollY > 100 && window.scrollY < 500) {
+                sectionAbout.classList.add(styles.actived)
+                sectionSkills.classList.remove(styles.actived)
+            } else if (window.scrollY >= 500) {
+                sectionAbout.classList.remove(styles.actived)
+                sectionSkills.classList.add(styles.actived)
+            } else {
+                sectionAbout.classList.remove(styles.actived)
+                sectionSkills.classList.remove(styles.actived) 
+            }
+            
+        })
+    }
+    
 }
