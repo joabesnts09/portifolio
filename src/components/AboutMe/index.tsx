450145427a01505d3317d13ba49abe48d24ac89b
joabesnts09 <joabesilva1233@gmail.com>
2024-05-17 10:54:28 -0300

    fix: link cv

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -50,7 +50,7 @@ export const AboutMe = () => {
                     </div>
 
                     <div className={styles.boxCv}>
-                        <a href='https://drive.google.com/file/d/1GzBo-R0EbTogD9hAbvACG2QwdbzQHnaX/view?usp=sharing' target='blank'>
+                        <a href='https://drive.google.com/file/d/1fbuwnB7nSp5BTgpd1s7I8HBS1MpvJsBD/view?usp=sharing' target='blank'>
                             <p>Currículo</p>
                             <Image src={imgDownload} width={22} alt='image download'/>
                         </a>
--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -3,44 +3,62 @@ import styles from '../components/Header/style.module.scss'
 import style from '../components/Main//BoxArroeUp/style.module.scss'
 
 export const useScroll = () => {
-    if (typeof window !== "undefined") {
+    if (typeof window !== 'undefined') {
         window.addEventListener('scroll', () => {
-            const header = document.querySelector('#header') as HTMLAnchorElement
-            const arrouUp = document.querySelector('#arrouUp') as HTMLAnchorElement
+            const header = document.querySelector(
+                '#header'
+            ) as HTMLAnchorElement
+            const arrouUp = document.querySelector(
+                '#arrouUp'
+            ) as HTMLAnchorElement
 
-            const secAbout = document.querySelector('#aboutme') as HTMLAnchorElement
-            const secSkill = document.querySelector('#skills') as HTMLAnchorElement
-            const secProjects = document.querySelector('#projects') as HTMLAnchorElement
-    
-            const sectionAbout = document.querySelector('#about') as HTMLAnchorElement
-            const sectionSkills = document.querySelector('#skill') as HTMLAnchorElement
-            const sectionProjects = document.querySelector('#project') as HTMLAnchorElement
-    
+            const secAbout = document.querySelector(
+                '#aboutme'
+            ) as HTMLAnchorElement
+            const secSkill = document.querySelector(
+                '#skills'
+            ) as HTMLAnchorElement
+            const secProjects = document.querySelector(
+                '#projects'
+            ) as HTMLAnchorElement
+
+            const sectionAbout = document.querySelector(
+                '#about'
+            ) as HTMLAnchorElement
+            const sectionSkills = document.querySelector(
+                '#skill'
+            ) as HTMLAnchorElement
+            const sectionProjects = document.querySelector(
+                '#project'
+            ) as HTMLAnchorElement
+
             header.classList.toggle(styles.scroll, window.scrollY > 0)
             arrouUp.classList.toggle(style.scrollArrow, window.scrollY > 600)
-            
-            if(window.scrollY > 100 && window.scrollY < secAbout.scrollHeight) { 
+
+            if (
+                window.scrollY > 100 &&
+                window.scrollY < secAbout.scrollHeight
+            ) {
                 sectionAbout.classList.add(styles.actived)
                 sectionSkills.classList.remove(styles.actived)
-            } else if (window.scrollY >= secAbout.scrollHeight  && window.scrollY < secAbout.scrollHeight + secSkill.scrollHeight) {
-                // console.log(secSkill.scrollHeight);
-                
+            } else if (
+                window.scrollY >= secAbout.scrollHeight &&
+                window.scrollY < secAbout.scrollHeight + secSkill.scrollHeight
+            ) {
                 sectionAbout.classList.remove(styles.actived)
                 sectionProjects.classList.remove(styles.actived)
                 sectionSkills.classList.add(styles.actived)
-            }  else if (window.scrollY >= secAbout.scrollHeight + secSkill.scrollHeight) {
-                console.log(secProjects.scrollHeight);
-                
+            } else if (
+                window.scrollY >=
+                secAbout.scrollHeight + secSkill.scrollHeight
+            ) {
                 sectionSkills.classList.remove(styles.actived)
                 sectionProjects.classList.add(styles.actived)
-            }else {
+            } else {
                 sectionAbout.classList.remove(styles.actived)
                 sectionSkills.classList.remove(styles.actived)
-                sectionProjects.classList.remove(styles.actived) 
+                sectionProjects.classList.remove(styles.actived)
             }
-            
-            
         })
     }
-    
 }
